20b5b0c506cf1c9f319d8f44912a0c7742d309f6
monasefdev <monasef77@gmail.com>
2024-04-07 21:00:33 +0000

    fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import AppLayout from "./ui/AppLayout";
+import { useEffect } from "react";
+import { getProducts } from "./services/FakeStoreApi";
 
 function App() {
+  useEffect(() => {
+    getProducts();
+  }, []);
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
---
+++ src/services/FakeStoreApi.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+export async function getProducts() {
+  try {
+    const { data } = await axios.get(BASE_URL + "/products");
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
